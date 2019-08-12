a113be9467cdf26d15d6323528b638f12212d619
Victor Malheiro <victorhugobarretos@gmail.com>
2019-08-12 18:06:37 -0300

    API GitHub - Users

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,20 @@
-module.exports = (
-    store(req,res){
-        return res.json({ message: "true"})
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(req, res){
+        const { username} = req.body;
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        const { name, bio, avatar_url: avatar } = response.data;
+        const dev = Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar: avatar
+        })
+
+        return res.json(dev);
     }
-);
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const DevController = require('./controllers/DevController');
 const routes = express.Router();
 
-routes.post('/devs',  DevController.stroe);
+routes.post('/devs',  DevController.store);
 
 module.exports = routes;
