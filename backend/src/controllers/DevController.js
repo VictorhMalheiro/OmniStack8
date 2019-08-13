f8ea07242ffe513ae1394d6a2c2756d773ea569c
Victor Malheiro <victorhugobarretos@gmail.com>
2019-08-13 13:07:06 -0300

    API Node.js Finalizada - Likes e Dislikes - Rotas - Match

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+    async index(req, res) {
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user }},// not equal
+                { _id: { $nin: loggedDev.likes }},//not in
+                { _id: { $nin: loggedDev.dislikes }},//not in
+            ],
+        })
+
+        return res.json(users);
+    },
+
     async store(req, res){
         const { username} = req.body;
 
