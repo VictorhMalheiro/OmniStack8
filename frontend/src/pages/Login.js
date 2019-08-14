b490a248e7a736689007db64c996512fbc85dafa
Victor <victorhugobarretos@gmail.com>
2019-08-14 02:56:50 -0300

    Finalização do Projeto Tindev

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import './Login.css';
+import React, { useState } from "react";
 
-import logo from '../assets/logo.svg';
+import "./Login.css";
 
-export default function Login() {
-    return (
-        <div className="login-container">
-            <form>
-                <img src={logo} alt="Tindev"/>
-                <input placeholder="Digite seu GitHub"/>
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
-    );
+import api from "../services/api";
+
+import logo from "../assets/logo.svg";
+
+export default function Login({ history }) {
+  const [username, setUsername] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      username
+    });
+    
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu GitHub"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
